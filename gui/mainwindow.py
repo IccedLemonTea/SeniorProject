2e057f82a60c5ceaeb4ece48bd8ce360d48beea6
Cooper White <cjw9009@g.rit.edu>
2026-01-27 19:06:57 -0500

    added imagery

--- gui/mainwindow.py
+++ gui/mainwindow.py
@@ -1,10 +1,12 @@
 # This Python file uses the following encoding: utf-8
 import sys
 import os
-from PySide6.QtWidgets import QApplication, QMainWindow, QFileDialog, QMessageBox, QListWidget, QListWidgetItem
+from PySide6.QtGui import QPixmap, QImage
+from PySide6.QtWidgets import QApplication, QMainWindow, QFileDialog, QMessageBox, QListWidget, QListWidgetItem, QLabel
 from PySide6.QtCore import QObject, Signal, QThread
 import LWIRImageTool as lit
 from workers.CalibrationWorker import CalibrationWorker
+import numpy as np
 
 # Important:
 # You need to run the following command to generate the ui_form.py file
@@ -18,20 +20,34 @@ class MainWindow(QMainWindow):
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
 
+
+        ### GUI Wide Variables ###
         self.list_of_files = []
-
-        # Connecting open image tab to trigger
-        self.ui.actionOpenImage.triggered.connect(self.open_image)
-        # Connecting Open Directory to trigger
-        self.ui.actionOpenBBDirectory.triggered.connect(self.open_blackbody_directory)
+        self.item_selected = ""
 
 
-    def calibration(self):
+        # Necessary for ease of access, limits the amount of text in the file list
+        self.home_dir = os.path.expanduser("~")
+
+        # Connecting actions to triggers
+        self.ui.actionOpenImage.triggered.connect(self.OpenImage)
+        self.ui.actionOpenBBDirectory.triggered.connect(self.OpenBlackbodyDirectory)
+        # self.ui.actionOpen_Other.triggered.connect(self.open_other) # TODO
+
+        self.ui.actionChooseCalibration.triggered.connect(self.Calibration)
+
+        # self.ui.actionView_Selected_Image.triggered.connect(self.ViewImage)
+
+
+        self.ui.widgetWorkspaceList.itemClicked.connect(self.OnItemClicked)
+
+    # TODO
+    def Calibration(self):
         print("Hello World!")
 
-        self.start_calibration(directory, filetype = 'rjpeg', rsr=None)
+        self.StartCalibration(directory, filetype = 'rjpeg', rsr=None)
 
-    def open_rsr_txt(self):
+    def OpenRSRText(self):
         rsr_path = QFileDialog.getOpenFileName(
         self,
         "Select RSR file path",
@@ -49,7 +65,7 @@ class MainWindow(QMainWindow):
             self.list_of_files.append(rsr_path)
         
             
-    def open_blackbody_directory(self):
+    def OpenBlackbodyDirectory(self):
         directory = QFileDialog.getExistingDirectory(
             self,
             "Select Directory",
@@ -68,24 +84,75 @@ class MainWindow(QMainWindow):
                 "Directory Selected",
                 f"You selected:\n{directory}"
                 )
-            self.list_of_files.append(directory)
+            
+            relative_path = os.path.relpath(directory, self.home_dir)
+            self.list_of_files.append(relative_path)
             list_item = QListWidgetItem()
-            list_item.setData(0,directory)
-            self.ui.fileList.addItem(list_item)
-            list_item_2 = QListWidgetItem()
-            list_item_2.setData(0,"Oops")
-            self.ui.fileList.addItem(list_item_2)
+            list_item.setData(0,relative_path)
+            self.ui.widgetWorkspaceList.addItem(list_item)
+
 
             
         
+    def OnItemClicked(self,item):
+        self.item_selected = item.text()
+        
+    # TODO
+    def OpenImage(self):
+        filepath = QFileDialog.getOpenFileName(
+            self,
+            "Select RSR file path",
+            "","*.txt;;All Files (*)"
+        )
 
-    # Open a singular image to view
-    # Input : self
-    # Action : Changes the main window and dispays the image that is selected.
-    def open_image(self):
-        print("Hello World!")
+        if not filepath:
+            QMessageBox.information(
+            self,
+            "No File Selected"
+            )
+            return
+        else:
+            QMessageBox.information(
+                self,
+                "File Selected",
+                f"You selected:\n{filepath}"
+                )
+        print(f"the filetype is {filepath[0]} and the type is {type(filepath[0])}")
+        relative_path = os.path.relpath(filepath[0], self.home_dir)
+        self.list_of_files.append(relative_path)
+        list_item = QListWidgetItem()
+        list_item.setData(0,relative_path)
+        self.ui.widgetWorkspaceList.addItem(list_item)
 
-    def update_progress(self, phase, current, total):
+        Factory = lit.ImageDataFactory()
+        image = Factory.create_from_file(filepath[0],"rjpeg")
+
+        arr = image.raw_counts
+
+        arr_disp = arr.astype(np.float32)
+        arr_disp -= arr_disp.min()
+        arr_disp /= arr_disp.max()
+        arr_disp *= 65535
+        arr_disp = arr_disp.astype(np.uint16)
+
+
+        h, w = arr.shape
+        bytes_per_line = w*2
+
+        q_image = QImage(
+            arr_disp.data,
+            w,
+            h,
+            bytes_per_line,
+            QImage.Format_Grayscale16
+        )
+
+        pixmap = QPixmap.fromImage(q_image)
+        self.ui.imagelabel.setPixmap(pixmap)
+        self.list_of_files.append(filepath[0])
+
+
+    def UpdateProgress(self, phase, current, total):
         percent = int((current / total) * 100)
 
         if phase == "loading":
@@ -97,7 +164,7 @@ class MainWindow(QMainWindow):
 
         self.ui.progressBar.setValue(percent)
 
-    def calibration_finished(self, cal_array):
+    def CalibrationFinished(self, cal_array):
         self.ui.progressBar.setValue(100)
         self.ui.progressBar.setFormat("Calibration complete")
 
@@ -110,7 +177,7 @@ class MainWindow(QMainWindow):
         )
         print(f"{self.calibration_data}")
 
-    def start_calibration(self, directory, filetype, rsr=None):
+    def StartCalibration(self, directory, filetype, rsr=None):
         self.thread = QThread()
         self.worker = CalibrationWorker(directory, filetype, rsr)
 
--- gui/ui_form.py
+++ gui/ui_form.py
@@ -16,21 +16,25 @@ from PySide6.QtGui import (QAction, QBrush, QColor, QConicalGradient,
     QIcon, QImage, QKeySequence, QLinearGradient,
     QPainter, QPalette, QPixmap, QRadialGradient,
     QTransform)
-from PySide6.QtWidgets import (QApplication, QListWidget, QListWidgetItem, QMainWindow,
-    QMenu, QMenuBar, QProgressBar, QSizePolicy,
-    QStatusBar, QWidget)
+from PySide6.QtWidgets import (QApplication, QLabel, QListWidget, QListWidgetItem,
+    QMainWindow, QMenu, QMenuBar, QProgressBar,
+    QSizePolicy, QStatusBar, QVBoxLayout, QWidget)
 
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         if not MainWindow.objectName():
             MainWindow.setObjectName(u"MainWindow")
-        MainWindow.resize(980, 681)
+        MainWindow.resize(980, 674)
         self.actionOpenImage = QAction(MainWindow)
         self.actionOpenImage.setObjectName(u"actionOpenImage")
         self.actionOpenBBDirectory = QAction(MainWindow)
         self.actionOpenBBDirectory.setObjectName(u"actionOpenBBDirectory")
-        self.actionPerformCalibration = QAction(MainWindow)
-        self.actionPerformCalibration.setObjectName(u"actionPerformCalibration")
+        self.actionChooseCalibration = QAction(MainWindow)
+        self.actionChooseCalibration.setObjectName(u"actionChooseCalibration")
+        self.actionOpen_Other = QAction(MainWindow)
+        self.actionOpen_Other.setObjectName(u"actionOpen_Other")
+        self.actionView_Selected_Image = QAction(MainWindow)
+        self.actionView_Selected_Image.setObjectName(u"actionView_Selected_Image")
         self.centralwidget = QWidget(MainWindow)
         self.centralwidget.setObjectName(u"centralwidget")
         self.progressBar = QProgressBar(self.centralwidget)
@@ -38,9 +42,32 @@ class Ui_MainWindow(object):
         self.progressBar.setGeometry(QRect(700, 580, 271, 51))
         self.progressBar.setValue(0)
         self.progressBar.setInvertedAppearance(False)
-        self.fileList = QListWidget(self.centralwidget)
-        self.fileList.setObjectName(u"fileList")
-        self.fileList.setGeometry(QRect(10, 10, 400, 400))
+        self.layoutWidget = QWidget(self.centralwidget)
+        self.layoutWidget.setObjectName(u"layoutWidget")
+        self.layoutWidget.setGeometry(QRect(10, 10, 201, 621))
+        self.verticalLayout = QVBoxLayout(self.layoutWidget)
+        self.verticalLayout.setObjectName(u"verticalLayout")
+        self.verticalLayout.setContentsMargins(0, 0, 0, 0)
+        self.labelWorkspaceFiles = QLabel(self.layoutWidget)
+        self.labelWorkspaceFiles.setObjectName(u"labelWorkspaceFiles")
+        font = QFont()
+        font.setPointSize(12)
+        self.labelWorkspaceFiles.setFont(font)
+        self.labelWorkspaceFiles.setAlignment(Qt.AlignCenter)
+
+        self.verticalLayout.addWidget(self.labelWorkspaceFiles)
+
+        self.widgetWorkspaceList = QListWidget(self.layoutWidget)
+        self.widgetWorkspaceList.setObjectName(u"widgetWorkspaceList")
+        font1 = QFont()
+        font1.setPointSize(7)
+        self.widgetWorkspaceList.setFont(font1)
+
+        self.verticalLayout.addWidget(self.widgetWorkspaceList)
+
+        self.imagelabel = QLabel(self.centralwidget)
+        self.imagelabel.setObjectName(u"imagelabel")
+        self.imagelabel.setGeometry(QRect(250, 20, 711, 551))
         MainWindow.setCentralWidget(self.centralwidget)
         self.statusbar = QStatusBar(MainWindow)
         self.statusbar.setObjectName(u"statusbar")
@@ -52,13 +79,18 @@ class Ui_MainWindow(object):
         self.menuFile.setObjectName(u"menuFile")
         self.menuEdit = QMenu(self.menubar)
         self.menuEdit.setObjectName(u"menuEdit")
+        self.menuView = QMenu(self.menubar)
+        self.menuView.setObjectName(u"menuView")
         MainWindow.setMenuBar(self.menubar)
 
         self.menubar.addAction(self.menuFile.menuAction())
         self.menubar.addAction(self.menuEdit.menuAction())
+        self.menubar.addAction(self.menuView.menuAction())
         self.menuFile.addAction(self.actionOpenImage)
         self.menuFile.addAction(self.actionOpenBBDirectory)
-        self.menuEdit.addAction(self.actionPerformCalibration)
+        self.menuFile.addAction(self.actionOpen_Other)
+        self.menuEdit.addAction(self.actionChooseCalibration)
+        self.menuView.addAction(self.actionView_Selected_Image)
 
         self.retranslateUi(MainWindow)
 
@@ -69,8 +101,13 @@ class Ui_MainWindow(object):
         MainWindow.setWindowTitle(QCoreApplication.translate("MainWindow", u"MainWindow", None))
         self.actionOpenImage.setText(QCoreApplication.translate("MainWindow", u"Open Image", None))
         self.actionOpenBBDirectory.setText(QCoreApplication.translate("MainWindow", u"Open Directory", None))
-        self.actionPerformCalibration.setText(QCoreApplication.translate("MainWindow", u"Perform Calibration", None))
+        self.actionChooseCalibration.setText(QCoreApplication.translate("MainWindow", u"Calibration", None))
+        self.actionOpen_Other.setText(QCoreApplication.translate("MainWindow", u"Open Other", None))
+        self.actionView_Selected_Image.setText(QCoreApplication.translate("MainWindow", u"View Selected Image", None))
+        self.labelWorkspaceFiles.setText(QCoreApplication.translate("MainWindow", u"Workspace Files", None))
+        self.imagelabel.setText(QCoreApplication.translate("MainWindow", u"TextLabel", None))
         self.menuFile.setTitle(QCoreApplication.translate("MainWindow", u"File", None))
-        self.menuEdit.setTitle(QCoreApplication.translate("MainWindow", u"Edit", None))
+        self.menuEdit.setTitle(QCoreApplication.translate("MainWindow", u"Calibrate", None))
+        self.menuView.setTitle(QCoreApplication.translate("MainWindow", u"View", None))
     # retranslateUi
 
