d7ac262689edbeead84082657e229a8afd873e94
Cooper White <cjw9009@g.rit.edu>
2026-01-30 18:39:28 -0500

    updated to have tabs

--- gui/mainwindow.py
+++ gui/mainwindow.py
@@ -36,19 +36,65 @@ class MainWindow(QMainWindow):
 
         self.ui.actionChooseCalibration.triggered.connect(self.Calibration)
 
-        # self.ui.actionView_Selected_Image.triggered.connect(self.ViewImage)
-
+        # self.ui.tabWidget.currentChanged.connect(self.OnTabChanged)
 
         self.ui.widgetWorkspaceList.itemClicked.connect(self.OnItemClicked)
 
+        # Disable tabs
+        for tab in [self.ui.imageTab, self.ui.calTab, self.ui.nedtTab, self.ui.stabilityTab]:
+            self.ui.tabWidget.setTabEnabled(
+                self.ui.tabWidget.indexOf(tab), False
+            )
+
+        # Force default tab (Image Preview)
+        self.ui.tabWidget.setCurrentIndex(
+            self.ui.tabWidget.indexOf(self.ui.imageTab)
+        )
+
+
+
+    # def OnTabChanged(self, index):
+    #     """Triggered when tab is switched"""
+
+    #     tab_text = self.ui.tabWidget.tabText(index)
+    #     # Act based on index or label
+    #     if index == 0:
+    #         # self.ViewImage()
+    #     elif index == 1:
+    #         # self.Calibration()
+
     # TODO
     def Calibration(self):
-        print("Hello World!")
+        self.StartCalibration(self.item_selected, filetype = 'rjpeg', rsr=None)
+    
+    def OpenImage(self):
+        filepath,_ = QFileDialog.getOpenFileName(
+            self,
+            "Select RSR file path",
+            "","All Files (*)"
+        )
 
-        self.StartCalibration(directory, filetype = 'rjpeg', rsr=None)
-
+        if not filepath:
+            QMessageBox.information(
+            self,
+            "No File Selected"
+            )
+            return
+        else:
+            QMessageBox.information(
+                self,
+                "File Selected",
+                f"You selected:\n{filepath}"
+                )
+        # relative_path = os.path.relpath(filepath[0], self.home_dir)
+        self.list_of_files.append(filepath[0])
+        list_item = QListWidgetItem()
+        list_item.setData(0,filepath[0])
+        self.ui.widgetWorkspaceList.addItem(list_item)
+        self.ui.tabWidget.setTabEnabled(self.ui.tabWidget.indexOf(self.ui.imageTab), True)
+    
     def OpenRSRText(self):
-        rsr_path = QFileDialog.getOpenFileName(
+        rsr_path,_ = QFileDialog.getOpenFileName(
         self,
         "Select RSR file path",
         "","*.txt;;All Files (*)"
@@ -62,11 +108,10 @@ class MainWindow(QMainWindow):
                 "RSR Selected",
                 f"You selected:\n{rsr_path}"
                 )
-            self.list_of_files.append(rsr_path)
-        
+            self.list_of_files.append(rsr_path)     
             
     def OpenBlackbodyDirectory(self):
-        directory = QFileDialog.getExistingDirectory(
+        directory,_ = QFileDialog.getExistingDirectory(
             self,
             "Select Directory",
             ""
@@ -86,46 +131,20 @@ class MainWindow(QMainWindow):
                 )
             
             relative_path = os.path.relpath(directory, self.home_dir)
-            self.list_of_files.append(relative_path)
+            self.list_of_files.append(directory)
             list_item = QListWidgetItem()
-            list_item.setData(0,relative_path)
+            list_item.setData(0,directory)
             self.ui.widgetWorkspaceList.addItem(list_item)
+            self.ui.tabWidget.setTabEnabled(self.ui.tabWidget.indexOf(self.ui.calTab), True)
+            self.ui.tabWidget.setTabEnabled(self.ui.tabWidget.indexOf(self.ui.stabilityTab), True)
 
-
-            
-        
     def OnItemClicked(self,item):
         self.item_selected = item.text()
-        
-    # TODO
-    def OpenImage(self):
-        filepath = QFileDialog.getOpenFileName(
-            self,
-            "Select RSR file path",
-            "","*.txt;;All Files (*)"
-        )
 
-        if not filepath:
-            QMessageBox.information(
-            self,
-            "No File Selected"
-            )
-            return
-        else:
-            QMessageBox.information(
-                self,
-                "File Selected",
-                f"You selected:\n{filepath}"
-                )
-        print(f"the filetype is {filepath[0]} and the type is {type(filepath[0])}")
-        relative_path = os.path.relpath(filepath[0], self.home_dir)
-        self.list_of_files.append(relative_path)
-        list_item = QListWidgetItem()
-        list_item.setData(0,relative_path)
-        self.ui.widgetWorkspaceList.addItem(list_item)
-
+    def ViewImage(self):
         Factory = lit.ImageDataFactory()
-        image = Factory.create_from_file(filepath[0],"rjpeg")
+        config = lit.ImageDataConfig(filename = self.item_selected, fileformat = "rjpeg")
+        image = Factory.create_from_file(config)
 
         arr = image.raw_counts
 
@@ -149,8 +168,29 @@ class MainWindow(QMainWindow):
 
         pixmap = QPixmap.fromImage(q_image)
         self.ui.imagelabel.setPixmap(pixmap)
-        self.list_of_files.append(filepath[0])
 
+    def StartCalibration(self, directory, filetype, rsr=None):
+        self.thread = QThread()
+        self.worker = CalibrationWorker(directory, filetype, rsr)
+
+        self.worker.moveToThread(self.thread)
+
+        # Wiring
+        self.thread.started.connect(self.worker.run)
+        self.worker.progress.connect(self.UpdateProgress)
+        self.worker.finished.connect(self.CalibrationFinished)
+
+
+        # Cleanup
+        self.worker.finished.connect(self.thread.quit)
+        self.worker.finished.connect(self.worker.deleteLater)
+        self.thread.finished.connect(self.thread.deleteLater)
+
+        # UI state
+        self.ui.progressBar.setValue(0)
+        self.ui.progressBar.setFormat("Starting calibration...")
+
+        self.thread.start()
 
     def UpdateProgress(self, phase, current, total):
         percent = int((current / total) * 100)
@@ -175,31 +215,7 @@ class MainWindow(QMainWindow):
             "Calibration Finished",
             "Blackbody calibration completed successfully."
         )
-        print(f"{self.calibration_data}")
-
-    def StartCalibration(self, directory, filetype, rsr=None):
-        self.thread = QThread()
-        self.worker = CalibrationWorker(directory, filetype, rsr)
-
-        self.worker.moveToThread(self.thread)
-
-        # Wiring
-        self.thread.started.connect(self.worker.run)
-        self.worker.progress.connect(self.update_progress)
-        self.worker.finished.connect(self.calibration_finished)
-
-
-        # Cleanup
-        self.worker.finished.connect(self.thread.quit)
-        self.worker.finished.connect(self.worker.deleteLater)
-        self.thread.finished.connect(self.thread.deleteLater)
-
-        # UI state
-        self.ui.progressBar.setValue(0)
-        self.ui.progressBar.setFormat("Starting calibration...")
-
-        self.thread.start()
-
+        self.tabWidget.setTabEnabled(self.tabWidget.indexOf(self.nedtTab), True)
 
 
 
